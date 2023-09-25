67bb8aebd45d69d11e5cbc4fc2f3a3ab046dbc21
priyanshugour70 <priyanshugournotes1@gmail.com>
2023-09-25 15:39:07 +0530

    first Commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,15 @@
-import Link from "next/link";
-import Head from 'next/head';
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "SpreadSheet | Dashboard of INurture",
+  description: "This is a Dashboard of INurture",
+  // other metadata
+};
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home Page</title>
-      </Head>
-      <main>
-        <h3>Welcome, To Spreadsheet Module..!</h3>
-        <Link href="/users">User of Database</Link>
-      </main>
+      Hello, This is Priyanshu Gour..!
     </>
-  )
+  );
 }
